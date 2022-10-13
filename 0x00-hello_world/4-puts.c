b1ef99cb7c14c0687dc27d93947e27aaed7ef5bd
MacBook Pro <jaykaynike@gmail.com>
2022-10-13 22:44:09 +0100

    auto add, commit and push

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - will print "Programming is like building a multilingual puzzle"
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
