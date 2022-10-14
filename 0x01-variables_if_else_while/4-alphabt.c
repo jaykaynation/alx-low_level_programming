4d4c4c6e8e64dc44377699da016bcc349b41f79d
MacBook Pro <jaykaynike@gmail.com>
2022-10-14 18:43:15 +0100

    auto add, commit and push

---
+++ 0x01-variables_if_else_while/4-alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - will print all alphabets except q and e
+ *
+ *Return: 0 for success
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
