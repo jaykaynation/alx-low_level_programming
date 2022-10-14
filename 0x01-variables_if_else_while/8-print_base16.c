1521b163ec77163ab257bcd3676117760f1b6167
MacBook Pro <jaykaynike@gmail.com>
2022-10-14 19:41:37 +0100

    auto add, commit and push

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - will print all the base16 numbers in lowercase
+ *
+ *Return: 0 for success
+ */
+int main(void)
+{
+	int x = 0;
+	char y = 'a';
+
+	while (x <= 9)
+	{
+		putchar(x + '0');
+		x++;
+	}
+
+	while (y <= 'f')
+	{
+		putchar(y);
+		y++;
+	}
+
+	return (0);
+}
