f2cd3fd32c437de501e2b0045254dd11c694a274
MacBook Pro <jaykaynike@gmail.com>
2022-10-14 13:08:45 +0100

    auto add, commit and push

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,19 +13,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	/* your code goes here */
-
 	if (n < 0)
 	{
 		printf("%d is %s\n", n, "negative");
 	}
-	else if (n == 0);
+	else if (n > 0)
 	{
-		printf("%d is %s\n", n, "zero");
+		printf("%d is %s\n", n, "positive");
 	}
 	else
 	{
-		printf("%d is %s\n", n, "positive");
+		printf("%d is %s\n", n, "zero");
 	}
 	return (0);
 }
