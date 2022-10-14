a5b8b0602de608631e4f57b57e6524a73c89f140
MacBook Pro <jaykaynike@gmail.com>
2022-10-14 17:55:30 +0100

    auto add, commit and push

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+/**
+ *main - prints alphabet in lower case
+ *
+ *Return: 0 for success
+ */
+int main(void)
+{
+	{
+	int c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		putchar(c);
+	}
+	printf("\n");
+	return (0);
+	}
+}
