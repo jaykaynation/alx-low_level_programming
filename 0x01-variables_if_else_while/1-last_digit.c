bdec0ef80b823d84ca81db3ca9d36085b119ebd7
MacBook Pro <jaykaynike@gmail.com>
2022-10-14 17:45:07 +0100

    auto add, commit and push

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - show the positional relation of a random number to 5
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n % 10 > 5)
+	{
+		printf("Last digit of %d is %d and it greater than 5/n", n, n %10);
+	}
+	else if (n % 10 < 6 && n % 10 != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	}
+	else if (n % 10 == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	}
+	return (0);
+}
