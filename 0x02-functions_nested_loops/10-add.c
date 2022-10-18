64a434bdf18a36361e5ecd0b170cabca30d74c21
MacBook Pro <jaykaynike@gmail.com>
2022-10-18 23:42:29 +0100

    auto add, commit and push

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,12 +3,11 @@
  * add - will return the sum of two integers
  * @x: is the first integer to be summed
  * @y: is the second integer to be summed
- * @z: is the value of the added integers
  * Return: 0 on success
  */
-int add(int x, int y, int z)
+int add(int x, int y)
 {
-	z = x + y;
+	int z = x + y;
 
 	return (z);
 }
