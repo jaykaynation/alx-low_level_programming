48f774c44b896ae46eca422d0785fccc21e7c5af
MacBook Pro <jaykaynike@gmail.com>
2022-10-18 22:37:17 +0100

    auto add, commit and push

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main."
+/**
+ * print_last_digit - will return the value of the last digit of a number
+ * @x: is the number in the function
+ * Return: 0 on success
+ */
+int print_last_digit(int x)
+{
+	x %= 10;
+	if (x < 0)
+	{
+		x = -x;
+	}
+	_putchar(x + '0');
+	return (x);
+}
