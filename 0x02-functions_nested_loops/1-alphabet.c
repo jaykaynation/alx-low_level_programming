86a7902fe1257997f390609093c85a0e63d272c8
MacBook Pro <jaykaynike@gmail.com>
2022-10-18 20:45:17 +0100

    auto add, commit and push

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+include "main.h"
+/**
+ *main - will print the alphabet in lowercase
+ *
+ *Return: 0 on success
+ */
+void print_alphabet(void);
+{
+	int x = 'a';
+	
+	while(x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+
+	return (0);
+}
