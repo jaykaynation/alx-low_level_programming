465879ae3061226e14653efd322b0e9eca55e9c9
MacBook Pro <jaykaynike@gmail.com>
2022-10-18 22:26:19 +0100

    auto add, commit and push

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - will print the absolute value of an integer
+ * @int: is the integer to get absolute value of
+ * Return: 0 on success
+ */
+int _abs(int)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n == 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
