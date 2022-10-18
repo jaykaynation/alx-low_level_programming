24d11ba0957d5a5206bdc5a5d463f509c582e9b5
MacBook Pro <jaykaynike@gmail.com>
2022-10-18 21:39:55 +0100

    auto add, commit and push

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - will check if the letter is lower case
+ * @c: is the lowercase char to print
+ * Return: 1 for lowercase and 0 otherwise
+ */
+int _islower(int c)
+{
+	int c = 'c';
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
