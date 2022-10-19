fe1a52c26301c21848bd1ec1ebb7fd96feaa9302
MacBook Pro <jaykaynike@gmail.com>
2022-10-19 16:40:09 +0100

    auto add, commit and push

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && a >= c)
+	if ((a >= b && b >= c) || (a >= c && c >= b))
 	{
-		largest = a;
+	largest = a;
 	}
-	else if (b >= a && a >= c)
+	else if ((b >= a && a >= c) || (b >= c && c >= a))
 	{
-		largest = b;
+	largest = b;
 	}
 	else
 	{
-		largest = c;
+	largest = c;
 	}
 
 	return (largest);
